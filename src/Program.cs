59afb9dfacce250f04a65db5a620d6c58800bc1e
JoseDeFreitas <joselocotote26@gmail.com>
2022-09-16 16:19:05 +0100

    Separate methods

--- src/Program.cs
+++ src/Program.cs
@@ -13,12 +13,6 @@ namespace DriveQuickstart
 {
     class Program
     {
-        static string[] Scopes = {
-            DriveService.Scope.Drive,
-            DriveService.Scope.DriveAppdata,
-            DriveService.Scope.DriveFile,
-        };
-        static string ApplicationName = "ManageFilesFromGoogleSheet";
         static string FolderName = "";
         static string TemplateSheet = "";
 
@@ -28,12 +22,80 @@ namespace DriveQuickstart
             Console.WriteLine("ManageFilesFromGoogleSheet");
             Console.WriteLine("══════════════════════════\n");
 
-            Console.Write("Input the name of the folder you want (won't be created if already exists): ");
-            string? FolderName = Console.ReadLine();
-            Console.WriteLine("\n");
-            Console.Write("Input the name of the template sheet (won't be created if already exists): ");
-            string? TemplateSheet = Console.ReadLine();
+            Console.WriteLine("Choose the option you want:");
+            Console.WriteLine("(1) Create a new folder/template registry.");
+            Console.WriteLine("(2) Update a folder/template registry.");
+            Console.WriteLine("(3) Delete the data of the application.\n");
 
+            Console.Write("Your option: ");
+
+            byte decision = 0;
+            try
+            {
+                decision = Convert.ToByte(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
+            switch (decision)
+            {
+                case 1:
+                    Console.WriteLine("Create a new folder/template registry.");
+
+                    Console.Write("Input the name of the folder: ");
+                    string? folderName = Console.ReadLine();
+                    string? templateName = Console.ReadLine();
+                    Console.Write("Input the name of the template file: ");
+
+                    RegistryStorage.SaveToRegistry(folderName!, templateName!);
+
+                    DriveManagement.ConnectToGoogle();
+                    DriveManagement.CreateFolder(folderName!);
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    Console.WriteLine("Delete the data of the application.");
+                    Console.WriteLine("All the data from the app will be deleted from the Windows registry.");
+                    Console.Write("Are you sure? (y/n): ");
+                    string? deleteOrNot = Console.ReadLine();
+
+                    if (deleteOrNot == "y")
+                        RegistryStorage.DeleteAppData();
+                    else if (deleteOrNot == "n")
+                        break;
+
+                    break;
+                default:
+                    Console.WriteLine("Only \"1\", \"2\", and \"3\" are valid options.");
+                    break;
+            }
+
+            return;
+        }
+    }
+
+    public class DriveManagement
+    {
+        static string[] Scopes = {
+            DriveService.Scope.Drive,
+            DriveService.Scope.DriveAppdata,
+            DriveService.Scope.DriveFile,
+        };
+        static string ApplicationName = "ManageFilesFromGoogleSheet";
+        static DriveService? Service;
+
+        public static void ConnectToGoogle()
+        {
             try
             {
                 UserCredential credential;
@@ -47,38 +109,51 @@ namespace DriveQuickstart
                         "admin",
                         CancellationToken.None,
                         new FileDataStore(credPath, true)).Result;
-                    Console.WriteLine($"Credential file saved to: {credPath}");
                 }
 
-                var service = new DriveService(new BaseClientService.Initializer
+                Service = new DriveService(new BaseClientService.Initializer
                 {
                     HttpClientInitializer = credential,
                     ApplicationName = ApplicationName
                 });
-
-                CreateFolder(service);
             }
             catch (FileNotFoundException e)
             {
                 Console.WriteLine(e.Message);
             }
-
-            return;
         }
 
-        static void CreateFolder(DriveService service)
+        public static void CreateFolder(string folderName)
         {
             var fileMetadata = new Google.Apis.Drive.v3.Data.File()
             {
-                Name = FolderName,
+                Name = folderName,
                 MimeType = "application/vnd.google-apps.folder"
             };
 
-            var request = service.Files.Create(fileMetadata);
+            var request = Service!.Files.Create(fileMetadata);
             request.Fields = "id";
             var file = request.Execute();
 
             Console.WriteLine("Folder ID: " + file.Id);
+        }
+    }
+
+    public class RegistryStorage
+    {
+        public static void SaveToRegistry(string folderName, string templateName)
+        {
+
+        }
+        
+        public static void ReadFromRegistry()
+        {
+
+        }
+
+        public static void DeleteAppData()
+        {
+
         }
     }
 }
